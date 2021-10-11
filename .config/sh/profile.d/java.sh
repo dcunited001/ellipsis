3c0a9c4c233455659cc11f33160c5e0e095f20c2
David Conner <dconner.pro@gmail.com>
2021-10-11 09:06:41 -0400

    Configure paths for Julia

--- .config/sh/profile.d/java.sh
+++ .config/sh/profile.d/java.sh
@@ -4,5 +4,4 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 
 # This sets Java Swing UI -> GTK
 #export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel $_JAVA_OPTIONS"
-#export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel {$_JAVA_OPTIONS}"
 # Java:1 ends here
