2499215e1d2ed51937e49a53ca1452169dba5f23
David Conner <dconner.pro@gmail.com>
2021-10-24 13:15:28 -0400

    yeh... so autoloading a guix profile
    may have caused my system instability. i assumed
    this was OK since i was only loading emacs (and a handful
    of GTK packages...)
    
    without it, systemd config required sourcing script/config, and i'm
    still not sure everything can interact properly with/from a running
    emacs, if its built via guix.
    
    nativecomp & pgtk are in AUR now. seems more stable. the
    nightmare is over?
    
    seriously ... crashes every 5 minutes and now I realllly understand the
    philosophy behind sixel and "all terminal all the things"

--- .config/sh/profile.d/guix.sh
+++ .config/sh/profile.d/guix.sh
@@ -3,8 +3,8 @@
 alias guix-all-profiles='find /gnu/store -maxdepth 1 -type d -name "*profile" -exec ls -al \{\} +'
 alias guix-main="$HOME/.config/guix/current/bin/guix"
 
-GUIX_PROFILE="$HOME/.guix-profile"
-. "$GUIX_PROFILE/etc/profile"
+#GUIX_PROFILE="$HOME/.guix-profile"
+#. "$GUIX_PROFILE/etc/profile"
 
 export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
 # Guix:1 ends here
