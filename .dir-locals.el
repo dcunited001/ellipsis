1bad045e73827cb39e020fbb6b245789059b50d5
David Conner <dconner.pro@gmail.com>
2022-01-15 12:51:36 -0500

    trying to test updated guix systems in geiser
    * this should reduce the feedback loop
    
    * but so many problems with an inconsistent environment

---
+++ .dir-locals.el
@@ -0,0 +1,22 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; ((nil . ((eval . (progn
+;;                    ;; NOTE: This is undefined until geiser loads
+;;                    ;; (setq-local geiser-guile-load-path
+;;                    ;;             (cons "/data/ecto/guix/guix" geiser-guile-load-path))
+;;
+;;                    ;; Also NOTE: updating geiser-guile-load-path in this way is not correct
+;;                    ;; - keeping for now to document modifying loadpath in dir-locals
+;;                    ;; - see https://github.com/hlissner/doom-emacs/pull/5415/commits/1b191b14575fb2bd73fe084d6c686666bd6fb4f3
+;;
+;;                    ;; from guix/guix
+;;                    (unless (boundp 'geiser-guile-load-path)
+;;                      (defvar geiser-guile-load-path '()))
+;;                    (make-local-variable 'geiser-guile-load-path)
+;;                    (require 'cl-lib)
+;;                    (cl-pushnew "/data/ecto/guix/guix" geiser-guile-load-path
+;;                                :test #'string-equal)
+;;                    )
+;;                ))
+;;       ))
