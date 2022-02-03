0297cdddcfcefebd7b36b45466e730667dd54fa2
David Conner <dconner.pro@gmail.com>
2022-02-03 04:23:20 -0500

    add .emacs.network profile

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,3 @@
 (("default" . ((user-emacs-directory . "~/.emacs.doom")))
- ("vanilla" . ((user-emacs-directory . "~/.emacs.vanilla"))))
+ ("vanilla" . ((user-emacs-directory . "~/.emacs.vanilla")))
+ ("network" . ((user-emacs-directory . "~/.emacs.network"))))
