293252919d70719928cb7ae1ed38da842e2b66e2
David Conner <dconner.pro@gmail.com>
2022-05-02 08:43:36 -0400

    updates to add fcitx5 service
    * anthy still not working though

--- .local/share/applications/fcitx5-launch-debug.sh
+++ .local/share/applications/fcitx5-launch-debug.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# [[file:../../../Bash.org::*FCITX][FCITX:1]]
+# [[file:~/.dotfiles/Bash.org::*FCITX][FCITX:1]]
 fcitx5 --verbose default=5 -D > $HOME/.cache/log/fcitx5.$(date +%s).log 2>&1
 # FCITX:1 ends here
