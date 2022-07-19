c8133dcb8202e99683dac625beab49ac407dad2a
David Conner <dconner.pro@gmail.com>
2022-07-19 14:46:54 -0400

    updates to build

--- ellipsis/systems/usb-gpg-tool.scm
+++ ellipsis/systems/usb-gpg-tool.scm
@@ -1,9 +1,9 @@
 ;;* Module
-(define-module (ellipsis systems usb-gpg-test)
+(define-module (ellipsis systems usb-gpg-tools)
+  #:use-module (gnu)
   #:use-module (gnu system)
   #:use-module (gnu system nss)
   #:use-module (gnu system pam)
-  #:use-module (gnu system install) ; TODO necessary to (inherit installtion-os)
 
   ;;** Basic Packages
   #:use-module (gnu packages version-control)
@@ -12,22 +12,28 @@
   ;; #:use-module (gnu packages curl)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages linux)
-  #:use-module (gnu packages mtools)    ;; for msdos file systems
+  #:use-module (gnu packages mtools) ;; for msdos file systems
   #:use-module (gnu packages file-systems)
 
   ;;** PGP Packages
+  #:use-module (gnu packages gnupg)
   #:use-module (gnu packages security-token)
 
   ;;** PGP Services
   #:use-module (gnu services authentication)
   #:use-module (gnu services security-token)
 
-  #:export (usb-gpg-tools))
+  #:export (usb-gpg-tools)
+
+  ;;NONFREE
+  ;; #:use-module (nongnu packages linux)
+  ;; #:use-module (nongnu system linux-initrd)
+  )
 
 ;; networking is [probably] needed for loopback
-(use-service-modules networking ssh)
-(use-service-modules udev security-token)
-(use-package-modules vim emacs linux mtools file-systems version-control)
+(use-service-modules networking ssh security-token)
+(use-package-modules wget vim emacs emacs-xyz)
+(use-package-modules linux mtools file-systems version-control)
 (use-package-modules screen ssh gnupg cryptsetup security-token)
 
 (define %my-user "dc")
@@ -49,15 +55,15 @@
    ;; to install on a system with just BIOS (e.g. a VM)
    (bootloader (bootloader-configuration
                 (bootloader grub-efi-bootloader)
-                (targets "" '("/dev/sda"))))
+                (targets "/dev/sda")))
    (file-systems (cons (file-system
                         (device (file-system-label "root"))
                         (mount-point "/")
                         (type "ext4"))
                        %base-file-systems))
 
-   ;; TODO: (bootloader...)
-   ;; TODO: (file-systems...)
+   ;; NONFREE
+   ;; (kernel linux)
 
    (kernel-arguments '("modprobe.blacklist=radeon"
                        ;; "quiet" ;; .....
@@ -66,46 +72,59 @@
 
    ;; TODO: users/groups (autologin to tty
 
-   (groups (cons* (user-group (name "plugdev") (system? #t))
-                  %base-groups))
-   (users (cons (user-account
-                 (name %my-user)
-                 (comment "Default User")
-                 (group "users")
-                 (supplementary-groups '("wheel"
-                                         "tty"
-                                         "plugdev")))))
+   (groups (append (list (user-group (name "plugdev") (system? #t)))
+                   %base-groups))
+   (users (append (list
+                   (user-account
+                    (name %my-user)
+                    (comment "Default User")
+                    (group "users")
+                    (supplementary-groups '("wheel"
+                                            "tty"
+                                            "plugdev"))))
+                  %base-user-accounts))
 
    (packages
-    (append (list exfat-utils
+    (append (list lvm2
+                  cryptsetup
+                  ntfs-3g
+                  exfat-utils
                   fuse-exfat
+                  openssh
+
                   git
                   ;; curl
                   stow
                   vim
+
                   emacs-no-x-toolkit
+                  emacs-better-defaults
+                  emacs-auto-complete
+                  emacs-hydra
+                  emacs-modus-themes
+                  emacs-dash
+                  emacs-lispy
+                  emacs-geiser
+                  emacs-geiser-guile
+                  emacs-guix
+                  emacs-yasnippet
+                  emacs-yasnippet-snippets
+
                   pcsc-lite
                   screen
                   gnupg
 
-                  ;; Yubikey tools
-                  python-yubikey-manager
-
-                  ;; pinentry
-                  pinentry-curses
-                  ;; pinentry-tty?
-                  ;; pinentry-qt?
-                  paperkey
-
-                  ;; pcscd
                   ccid
-
-                  ;; for host-side u2f protocol (should include udev rules)
+                  yubikey-personalization
+                  python-yubikey-manager
                   libu2f-host
+
+                  pinentry-tty
+                  paperkey
 
                   wget
                   )
-            %base-packages-disk-utilites
+            %base-packages-disk-utilities
             %base-packages))
 
    (services
@@ -122,4 +141,4 @@
             %my-services
             ))))
 
-;; usb-gpg-tools
+usb-gpg-tools
