ccb9e23d1768489e9c9945e9871d38185ed95c7f
David Conner <dconner.pro@gmail.com>
2022-07-24 16:09:53 -0400

    starting to look into this stuff

---
+++ ellipsis/packages/emacs-xyz.scm
@@ -0,0 +1,18 @@
+(define-module (ellipsis packages emacs-xyz)
+  #:use-module ((guix licenses) #:prefix license:)
+
+  #:use-module (guix gexp)
+  #:use-module (guix utils)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix build-system emacs)
+
+  #:use-module (gnu packages base)
+
+  #:use-module (srfi srfi-1)
+  )
+
+
+;; https://github.com/jobbflykt/x509-mode
+;; https://guix.gnu.org/manual/en/guix.html#emacs_002dbuild_002dsystem
