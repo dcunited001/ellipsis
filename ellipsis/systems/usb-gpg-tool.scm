6a0893f2d7ae7c40a0c730ebadf79710e58a598c
David Conner <dconner.pro@gmail.com>
2022-08-05 05:27:34 -0400

    add utils to desktop & usb-gpg-tool

--- ellipsis/systems/usb-gpg-tool.scm
+++ ellipsis/systems/usb-gpg-tool.scm
@@ -18,7 +18,6 @@
   ;;** PGP Packages
   #:use-module (gnu packages gnupg)
   #:use-module (gnu packages security-token)
-  #:use-module (gnu packages tls)
 
   ;;** PGP Services
   #:use-module (gnu services authentication)
@@ -39,9 +38,9 @@
 
 ;; networking is [probably] needed for loopback
 (use-service-modules networking ssh security-token)
-(use-package-modules wget vim emacs emacs-xyz)
-(use-package-modules linux time mtools file-systems version-control)
-(use-package-modules screen ssh gnupg cryptsetup security-token certs)
+(use-package-modules wget screen password-utils vim emacs emacs-xyz)
+(use-package-modules linux time mtools file-systems disk version-control)
+(use-package-modules ssh gnupg cryptsetup security-token tls certs)
 
 (define %my-user "dc")
 
@@ -64,109 +63,120 @@
 ;;** Image
 (define usb-gpg-tools
   (operating-system
-   (host-name "usbgpgtool")
-   (timezone "America/New_York")
-   (locale "en_US.utf8")
+    (host-name "usbgpgtool")
+    (timezone "America/New_York")
+    (locale "en_US.utf8")
 
-   ;; to install on a system with just BIOS (e.g. a VM)
-   (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (targets "/dev/sda")))
-   (file-systems (cons (file-system
-                        (device (file-system-label "root"))
-                        (mount-point "/")
-                        (type "ext4"))
-                       %base-file-systems))
+    ;; to install on a system with just BIOS (e.g. a VM)
+    (bootloader (bootloader-configuration
+                 (bootloader grub-efi-bootloader)
+                 (targets "/dev/sda")))
+    (file-systems (cons (file-system
+                          (device (file-system-label "root"))
+                          (mount-point "/")
+                          (type "ext4"))
+                        %base-file-systems))
 
-   ;; NONFREE
-   ;; (kernel linux)
+    ;; NONFREE
+    ;; (kernel linux)
 
-   (kernel-arguments '("modprobe.blacklist=radeon"
-                       ;; "quiet" ;; .....
-                       ;; "net.iframes=0"
-                       ))
+    (kernel-arguments '("modprobe.blacklist=radeon"
+                        ;; "quiet" ;; .....
+                        ;; "net.iframes=0"
+                        ))
 
-   ;; TODO: users/groups (autologin to tty
+    ;; TODO: users/groups (autologin to tty
 
-   (groups (append (list (user-group (name "plugdev") (system? #t)))
-                   %base-groups))
-   (users (append (list
-                   (user-account
-                    (name %my-user)
-                    (comment "Default User")
-                    (group "users")
-                    (supplementary-groups '("wheel"
-                                            "tty"
-                                            "plugdev"))))
-                  %base-user-accounts))
+    (groups (append (list (user-group (name "plugdev") (system? #t)))
+                    %base-groups))
+    (users (append (list
+                    (user-account
+                     (name %my-user)
+                     (comment "Default User")
+                     (group "users")
+                     (supplementary-groups '("wheel"
+                                             "tty"
+                                             "plugdev"))))
+                   %base-user-accounts))
 
-   (packages
-    (append (list lvm2
-                  cryptsetup
-                  ntfs-3g
-                  exfat-utils
-                  fuse-exfat
+    ;; misc packages:
+    ;; f3: test flash storage
+    ;; paperkey: print keys to paper
+    ;; certdata2pem: convert between cert formats
+    ;; datefudge: mock system time to set arbitrary cert start times
+    ;; exfat-utils: work with FAT disks
+    ;; pwsafe: manage passwords
 
-                  wget
-                  git
-                  stow
-                  vim
+    (packages
+     (append (list lvm2
+                   cryptsetup
+                   ntfs-3g
+                   exfat-utils
+                   fuse-exfat
+                   f3
 
-                  emacs-no-x-toolkit
-                  emacs-better-defaults
-                  ;; emacs-with-profile
-                  emacs-auto-complete
-                  emacs-hydra
-                  emacs-modus-themes
-                  emacs-dash
-                  emacs-lispy
-                  emacs-geiser
-                  emacs-geiser-guile
-                  emacs-ac-geiser
-                  emacs-guix
-                  emacs-yasnippet
-                  emacs-yasnippet-snippets
+                   wget
+                   git
+                   stow
+                   vim
 
-                  screen
-                  openssh
-                  openssl
+                   emacs-no-x-toolkit
+                   emacs-better-defaults
+                   ;; emacs-with-profile
+                   emacs-auto-complete
+                   emacs-hydra
+                   emacs-modus-themes
+                   emacs-dash
+                   emacs-lispy
+                   emacs-geiser
+                   emacs-geiser-guile
+                   emacs-ac-geiser
+                   emacs-guix
+                   emacs-yasnippet
+                   emacs-yasnippet-snippets
 
-                  pcsc-lite
-                  gnupg
+                   screen
+                   openssh
+                   openssl
 
-                  ccid
-                  yubico-piv-tool
-                  yubikey-personalization
-                  python-yubikey-manager
-                  libu2f-host
+                   pcsc-lite
+                   gnupg
 
-                  pinentry-tty
-                  paperkey
-                  datefudge
+                   ccid
+                   yubico-piv-tool
+                   yubikey-personalization
+                   python-yubikey-manager
+                   libu2f-host
 
-                  le-certs
-                  nss-certs
+                   pinentry-tty
+                   paperkey
+                   datefudge
 
-                  step-ca-bin
-                  step-cli-bin
-                  certdata2pem
-                  ;; desec-certbot-hook
-                  )
-            %base-packages-disk-utilities
-            %base-packages))
+                   shroud-nox
 
-   (services
-    (append (list
-             ;; (dhcp-client-service-type)
-             (service pcscd-service-type)
-             ;; NOTE: udev rules should automatically be in place
-             ;; (udev-rules-service
-             ;;  (udev-rule "90-libu2f.rules"
-             ;;             (string-append "")))
+                   le-certs
+                   nss-certs
 
-             )
+                   step-ca-bin
+                   step-cli-bin
+                   certdata2pem
+                   ;; desec-certbot-hook
+                   )
+             %base-packages-disk-utilities
+             %base-packages))
 
-            %my-services
-            ))))
+    (services
+     (append (list
+              ;; (dhcp-client-service-type)
+              (service pcscd-service-type)
+              ;; NOTE: udev rules should automatically be in place
+              ;; (udev-rules-service
+              ;;  (udev-rule "90-libu2f.rules"
+              ;;             (string-append "")))
+
+              )
+
+             %my-services
+             ))))
 
 usb-gpg-tools
