c815293d79065cdba5b5c39513fced786776f1bb
David Conner <dconner.pro@gmail.com>
2022-08-21 20:41:33 -0400

    add package for emacs-x509-mode

--- ellipsis/packages/emacs-xyz.scm
+++ ellipsis/packages/emacs-xyz.scm
@@ -13,11 +13,40 @@
   #:use-module (srfi srfi-1)
   )
 
+;; open /etc/ssl/certs to test
+(define-public emacs-x509-mode
+  (package
+    (name "emacs-x509-mode")
+    (version "20220819.541")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://melpa.org/packages/x509-mode-" version ".tar"))
+
+              (sha256
+               (base32
+                "1fcgcx01phazmk58ijlsnbshcr20wns6pnv66n25vwmdi1kp1chw"))))
+    (build-system emacs-build-system)
+    (arguments
+     `(#:include (cons* "^keywords.txt"
+                        "^keyword\\+constant.txt"
+                        "^constants.txt"
+                        "^long-name.txt"
+                        %default-include)))
+    (home-page "https://github.com/jobbflykt/x509-mode")
+    (synopsis "Major mode for viewing certificates, CRLs, and other PKI-related files.")
+    (description "Major mode for viewing certificates, CRLs, and other PKI-related files.
+
+Uses OpenSSL for viewing PEM and DER encoded PKI entities.")
+    (license #f)))
 
 ;; https://github.com/jobbflykt/x509-mode
 ;; https://guix.gnu.org/manual/en/guix.html#emacs_002dbuild_002dsystem
-;;
-;; melpa recipe:
-;; (x509-mode :repo "jobbflykt/x509-mode"
-;;            :fetcher github
-;;            :files (:defaults "*.txt"))
+
+;; `(#:phases
+;;   (modify-phases %standard-phases
+;;     (add-after 'install 'install-keywords
+;;       (lambda* (#:key inputs outputs #:allow-other-keys)
+;;         (let* ((out (assoc-ref outputs "out"))
+;;                (site-lisp (string-append out "/share/emacs/site-lisp"))
+;;                (libdir (string-append site-lisp "/x509-mode-" version)))
+;;           (copy-file "keyword.txt"))))))
