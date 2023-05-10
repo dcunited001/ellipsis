48befea7a368ce2baf3952d9c4dcc51fed7419b5
David Conner <dconner.pro@gmail.com>
2023-05-10 06:07:35 -0400

    python, fbcon bash script, kde updates

---
+++ .screenlayout/kratos-1440-LRR.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x320 --rotate right --output DisplayPort-1 --mode 2560x1440 --pos 1080x0 --rotate left --output DisplayPort-2 --primary --mode 2560x1440 --pos 2520x0 --rotate left --output HDMI-A-0 --off
---
+++ .screenlayout/kratos-4k-LRR.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x960 --rotate right --output DisplayPort-1 --mode 3840x2160 --pos 1080x0 --rotate left --output DisplayPort-2 --primary --mode 2560x1440 --pos 3240x640 --rotate left --output HDMI-A-0 --off
