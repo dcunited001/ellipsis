78892e004ed48c1fef4cb09fc9576834ddd59054
David Conner <dconner.pro@gmail.com>
2023-05-22 09:21:19 -0400

    added some packages that don't build for hyprland
    * their discord server is wierd. don't accidentally reply to the
      wrong person who seems like they might know something

---
+++ ellipsis/packages/freedesktop.scm
@@ -0,0 +1,114 @@
+(define-module (ellipsis packages freedesktop)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix gexp)
+  #:use-module (guix utils)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix build-system meson)
+
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages ninja)
+  ;; #:use-module (ice-9 pretty-print)
+
+  #:use-module (ellipsis packages wm)
+
+  #:use-module (srfi srfi-1))
+
+
+;; NOTE: the hyprland packages aren't building.
+;; undefined reference to `drmGetDeviceFromDevId'
+
+;; lib,
+;; stdenv,
+;; makeWrapper,
+;; meson,
+;; ninja,
+;; pkg-config,
+;; wayland-protocols,
+;; wayland-scanner,
+;; hyprland-share-picker,
+;; grim,
+;; slurp,
+;; hyprland-protocols,
+;; inih,
+;; libdrm,
+;; libuuid,
+;; mesa,
+;; pipewire,
+;; systemd,
+;; wayland,
+;; version ? "git",
+
+;; nativeBuildInputs = [
+;;   meson
+;;   ninja
+;;   pkg-config
+;;   wayland-scanner
+;;   makeWrapper
+;; ];
+;; buildInputs = [
+;;   hyprland-protocols
+;;   inih
+;;   libdrm
+;;   libuuid
+;;   mesa
+;;   pipewire
+;;   systemd
+;;   wayland
+;;   wayland-protocols
+;; ];
+
+;; meson-build-system: extends from gnu-build-system
+
+;; gnu build phases:
+;; configure
+;; build
+;; check
+
+;; meson run phases:
+;; fix-runpath
+;; glib-or-gtk-wrap
+;; glib-or-gtk-compile-schemas
+
+(define-public xdg-desktop-portal-hyprland
+  (package
+    (name "xdg-desktop-portal-hyprland")
+    (version "0.3.1")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/hyprwm/xdg-desktop-portal-hyprland/archive/refs/tags/v"
+                    version "/v" version ".tar.gz"))
+              (sha256
+               (base32
+                "1cvga9z261kybpzrw57jagrgxhsnln8pm63n2b8flk7r6x7lkb7c"))))
+    (build-system meson-build-system)
+    ;; (propagated-inputs (list ))
+    (inputs (list pipewire
+                  libdrm
+                  `(,util-linux "lib")
+                  ;; elogind
+                  basu
+                  libinih
+                  wayland))
+    (native-inputs (list pkg-config
+                         cmake
+                         ninja
+                         wayland-protocols-next
+                         hyprland-protocols
+                         mesa))
+
+    (home-page "https://github.com/hyprwm/xdg-desktop-portal-hyprland")
+    (synopsis "xdg-desktop-portal backend for Hyprland")
+    (description "")
+    (license license:bsd-3)))
+
+;; https://github.com/hyprwm/xdg-desktop-portal-hyprland/releases/download/v0.3.1/v0.3.1.tar.gz
--- ellipsis/utils.scm
+++ ellipsis/utils.scm
@@ -13,3 +13,13 @@
   )
 
 (define util "util")
+
+;; (define-public (remove-pulseaudio-service services-list)
+;;   (remove (lambda (service)
+;;             (eq? (service-kind service) pulseaudio-service-type))
+;;           services-list))
+
+;; (define-public (remove-gdm-service services-list)
+;;   (remove (lambda (service)
+;;             (eq? (service-kind service) gdm-service-type))
+;;           services-list))
