94721c7843e7750caced4bf0f753a8b6515aed31
David Conner <aionfork@gmail.com>
2023-05-25 22:25:53 -0400

    refactor a bit and create an x11 system

--- dc/systems/base.scm
+++ dc/systems/base.scm
@@ -35,6 +35,12 @@
   (plain-file "nntpserver.conf"
               "news.gmane.io"))
 
+(define-public %dc-nntp-service
+  ;; for GNUS
+  (simple-service 'nntp-config etc-service-type
+                  (list `("nntpserver"
+                          ,%dc-nntpserver))))
+
 (define-public %dc-base-groups
   (cons* (user-group (name "realtime") (system? #t))
          ;; created by service
@@ -46,6 +52,87 @@
          (user-group (name "dc") (id 1000))
          (remove (lambda (g) (equal? (user-group-name g) "users"))
                  %base-groups)))
+
+(define-public %dc-docker-service
+  (service docker-service-type
+           (docker-configuration
+            (enable-proxy? #f))))
+
+(define-public %dc-libvirt-service
+  (service libvirt-service-type
+           (libvirt-configuration
+            (unix-sock-group "libvirt")
+            (tls-port "16555"))))
+
+(define-public %dc-virtlog-service
+  (service virtlog-service-type
+           (virtlog-configuration
+            ;; (max-clients 1024) ;; default
+            (max-size (* 32 (expt 1024 2))))))
+
+(define-public %dc-cups-service
+  (service cups-service-type
+           (cups-configuration
+            (web-interface? #t)
+            ;; TODO ssl-options? TLS 1.0+
+            (extensions
+             (list cups-filters
+                   epson-inkjet-printer-escpr
+                   hplip-minimal)))))
+
+(define-public %dc-pam-limits-service
+  ;; TODO: (service pam-limits-service-type
+  (pam-limits-service
+   (list
+    (pam-limits-entry "@realtime" 'both 'rtprio 99)
+    (pam-limits-entry "@realtime" 'both 'nice -19)
+    (pam-limits-entry "@realtime" 'both 'memlock 'unlimited))))
+
+;; TODO: this won't have environment vars set (for ssh)
+(define-public %dc-unattended-upgrade-service-type
+  (service
+   unattended-upgrade-service-type
+   (unattended-upgrade-configuration
+    (schedule "30 2 * * 0")
+    (channels #~(list
+                 (channel
+                  (name 'nonguix)
+                  (url "https://gitlab.com/nonguix/nonguix")
+                  (branch "master"))
+                 %default-channels))
+    ;; (operating-system-file
+    ;;  (file-append
+    ;;   (local-file "." "systems-dir" #:recursive? #t)
+    ;;   (string-append
+    ;;    "/root/.config/guix/systems/" %host-name ".scm")))
+    (system-expiration (* 6 7 24 3600)))))
+
+(define-public %dc-nonguix-substitutes-service
+  (simple-service
+   'add-nonguix-substitutes
+   guix-service-type
+   (guix-extension
+    (substitute-urls
+     (append (list "https://substitutes.nonguix.org")
+             %default-substitute-urls))
+    (authorized-keys
+     (append (list (plain-file "nonguix.pub" "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
+             %default-authorized-guix-keys)))))
+
+(define-public %dc-extra-file-env
+  (extra-special-file "/usr/bin/env"
+                      (file-append coreutils "/bin/env")))
+
+(define-public %dc-extra-file-ld-linux
+  (extra-special-file "/lib64/ld-linux-x86-64.so.2"
+                      (file-append glibc "/lib/ld-linux-x86-64.so.2")))
+
+;; (define-public %dc-extra-file-flatpak
+;;   (extra-special-file
+;;    "/etc/flatpak/installations.d"
+;;    (file-union "installations.d"
+;;                `(("steam.conf" ,(local-file "flatpak/steam.conf"))
+;;                  ("agenda.conf" ,(local-file "flatpak/agenda.conf"))))))
 
 ;; add libvirt & docker to the users who need it
 (define-public %dc-my-groups
@@ -82,6 +169,52 @@
                   "\n"
                   "ACTION==\"add\", SUBSYSTEM==\"backlight\", "
                   "RUN+=\"/run/current-system/profile/bin/chmod g+w /sys/class/backlight/%k/brightness\"")))
+
+;; DNS is wierd on some networks, other times my network is disabled
+;; i would like to be sure that it's definitely the latter
+(define-public %dc-ntp-service
+  (service ntp-service-type
+           (ntp-configuration
+            (servers
+             (list (ntp-server
+                    (type 'pool)
+                    (address "1.us.pool.ntp.org")
+                    (options '("iburst")))
+                   (ntp-server
+                    (type 'pool)
+                    (address "2.us.pool.ntp.org")
+                    (options '("iburst")))
+                   (ntp-server
+                    (type 'pool)
+                    (address "3.us.pool.ntp.org")
+                    (options '("iburst"))))))))
+
+(define-public %dc-network-manager-service
+  (service network-manager-service-type
+           (network-manager-configuration
+            (vpn-plugins
+             (list network-manager-openvpn)))))
+
+(define-public %dc-ras-daemon-service
+  (service rasdaemon-service-type
+                (rasdaemon-configuration (record? #t))))
+
+(define-public %dc-i2c-packages
+  (list i2c-tools
+        hw-probe
+        ddcci-driver-linux
+        ddcutil
+        ;; ddcui
+        ))
+
+(define-public %dc-packages-xorg-only
+  (list
+
+   ;; required for wacom
+   ;; - libwacom modifies udev rules & must be in system config
+   xf86-input-libinput
+   xf86-input-wacom
+   libwacom))
 
 (define-public %dc-desktop-packages
   (list
@@ -147,11 +280,3 @@
 
    gvfs
    nss-certs))
-
-(define-public %dc-packages-xorg-only
-  (list
-
-   ;; required for wacom
-   ;; - libwacom modifies udev rules & must be in system config
-   xf86-input-wacom
-   libwacom))
