a53a4a4ec8d2e6ce4f3daac5fdd83145252b8c8c
David Conner <aionfork@gmail.com>
2023-05-31 04:36:01 -0400

    add script for guix, remove d-feet

--- .xdg_shim.eg.sh
+++ .xdg_shim.eg.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 # [[file:Bash.org::*XDG Shim][XDG Shim:1]]
 export XDG_CONFIG_HOME=$HOME/.config
-#export XDG_CONFIG_DIRS=/etc/xdg
-#export XDG_DATA_HOME=$HOME/.local/share
-#export XDG_DATA_DIRS=/usr/local/share/:/usr/share/
-#export XDG_STATE_HOME=$HOME/.local/state
-#export XDG_CACHE_HOME=$HOME/.cache
 # XDG Shim:1 ends here
