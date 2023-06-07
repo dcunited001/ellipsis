97659ce05b7ba52620ca71afb4007aca1b7c15b1
David Conner <aionfork@gmail.com>
2023-06-07 03:40:30 -0400

    start nyxt 3 config

---
+++ .config/nyxt/config.lisp
@@ -0,0 +1,43 @@
+;;* Nyxt
+
+;;** Editor
+(defmethod customize-instance ((browser browser) &key)
+           (setf (slot-value browser 'theme) theme:+dark-theme+)
+           (setf (slot-value browser 'external-editor-program) '("gmacsclient" "-c")))
+
+;;** Keybindings
+(define-configuration
+ buffer
+ ((default-modes
+   (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+
+;;** Search
+(defvar *dc/search-engines*
+  (list
+   '("g" "https://google.com/search?q=~a" "https://google.com")
+   '("doi" "https://dx.doi.org/~a" "https://dx.doi.org/")
+   '("py" "https://docs.python.org/3/search.html?q=~a"
+     "https://docs.python.org/3")
+   '("ansd" "https://docs.ansible.com/ansible/latest/index.html#stq=~a&stp=1"
+     "https://docs.ansible.com/")
+   '("ansg" "https://galaxy.ansible.com/search?deprecated=false&keywords=~a&order_by=-download_count&page=1"
+     "https://galaxy.ansible.com/")
+   '("gh" "https://github.com/~a" "Github (URL)")
+   '("ghi" "https://github.com/search?q=~a&type=issues" "Github (Issues)")
+   '("ghp" "https://github.com/search?q=~a&type=pullrequests" "Github (Pull Reqs)")
+   '("ght" "https://github.com/search?q=~a&type=topics" "Github (Topics)")
+   '("npm" "https://www.npmjs.com/search?q=~a"
+     "https://www.npmjs.com/"))
+  "List of search engines.")
+
+;; TODO: consider using ©hárß for urls without search
+;; '("á©" "https://galaxy.ansible.com/community") ; no URL for community
+
+(define-configuration
+ context-buffer
+ "Go through the search engines above and make-search-engine out of them."
+ ((search-engines
+   (append
+    (mapcar (lambda (engine) (apply 'make-search-engine engine))
+            *dc/search-engines*)
+    %slot-default%))))
