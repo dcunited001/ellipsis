7911b813a4539f150d8a28fb07bf9869189ddf30
David Conner <aionfork@gmail.com>
2023-06-09 20:17:24 -0400

    formatting in emacs-x509

--- ellipsis/packages/emacs-xyz.scm
+++ ellipsis/packages/emacs-xyz.scm
@@ -1,36 +1,36 @@
 (define-module (ellipsis packages emacs-xyz)
   #:use-module ((guix licenses) #:prefix license:)
-
   #:use-module (guix gexp)
   #:use-module (guix utils)
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (guix build-system emacs)
-
   #:use-module (gnu packages)
   #:use-module (gnu packages tls)
-
   #:use-module (srfi srfi-1))
 
 (define-public emacs-x509-mode
   (package
     (name "emacs-x509-mode")
     (version "20230430.1518")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://melpa.org/packages/x509-mode-" version ".tar"))
-              (sha256
-               (base32
-                "195gn14qn22aqclwz2i9w2qq9bxabwcasfkna1fj41jwv2bpbykc"))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        (string-append "https://melpa.org/packages/x509-mode-" version ".tar"))
+       (sha256
+        (base32
+         "195gn14qn22aqclwz2i9w2qq9bxabwcasfkna1fj41jwv2bpbykc"))))
     (build-system emacs-build-system)
     ;; (propagated-inputs `(("openssl" ,openssl)))
     (arguments
-     `(#:include (cons* "^keywords.txt"
-                        "^keyword\\+constant.txt"
-                        "^constants.txt"
-                        "^long-name.txt"
-                        %default-include)))
+     `(#:include
+       (cons* "^keywords.txt"
+              "^keyword\\+constant.txt"
+              "^constants.txt"
+              "^long-name.txt"
+              %default-include)))
     (home-page "https://github.com/jobbflykt/x509-mode")
     (synopsis "Major mode for viewing certificates, CRLs, and other PKI-related files")
     (description "Major mode for viewing certificates, CRLs, and other PKI-related files.
