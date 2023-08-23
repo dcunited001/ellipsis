4787819f58f634c10796cac0e45968237d4d8b54
David Conner <aionfork@gmail.com>
2023-08-22 20:35:44 -0400

    move guix dir-locals to channel root

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,30 +1,124 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
+((nil
+  (fill-column . 78)
+  (tab-width   .  8)
+  (sentence-end-double-space . t)
 
-;; TODO: try org-babel-tangle-body-hook
-;; - none of the org-babel-tangle-body-hooks are passed variables when run
+  (eval . (let ((guix-snippet-dir "/data/ecto/guix/guix/etc/snippets"))
+            (and (file-directory-p guix-snippet-dir)
+                 (unless (member guix-snippet-dir yas-snippet-dirs)
+                   (setq-local yas-snippet-dirs
+                               (cons guix-snippet-dir yas-snippet-dirs)))))))
 
-;; ((nil . (
-;;          (org-babel-tangle-comment-format-beg . "** [[%link][%source-name]]")
-;;         )
-;;       ))
+ (shell-mode
+  (eval add-hook 'shell-mode-hook 'guix-build-log-minor-mode))
 
+ (scheme-mode
+  (eval add-hook 'scheme-mode-hook 'guix-devel-mode)
 
-;;          (eval . (progn
-;;                    ;; NOTE: This is undefined until geiser loads
-;;                    ;; (setq-local geiser-guile-load-path
-;;                    ;;             (cons "/data/ecto/guix/guix" geiser-guile-load-path))
-;;
-;;                    ;; Also NOTE: updating geiser-guile-load-path in this way is not correct
-;;                    ;; - keeping for now to document modifying loadpath in dir-locals
-;;                    ;; - see https://github.com/hlissner/doom-emacs/pull/5415/commits/1b191b14575fb2bd73fe084d6c686666bd6fb4f3
-;;
-;;                    ;; from guix/guix
-;;                    (unless (boundp 'geiser-guile-load-path)
-;;                      (defvar geiser-guile-load-path '()))
-;;                    (make-local-variable 'geiser-guile-load-path)
-;;                    (require 'cl-lib)
-;;                    (cl-pushnew "/data/ecto/guix/guix" geiser-guile-load-path
-;;                                :test #'string-equal)
-;;                    )
-;;                )
+  (indent-tabs-mode . nil)
+
+  (eval . (put 'eval-when 'scheme-indent-function 1))
+  (eval . (put 'call-with-prompt 'scheme-indent-function 1))
+  (eval . (put 'test-assert 'scheme-indent-function 1))
+  (eval . (put 'test-assertm 'scheme-indent-function 1))
+  (eval . (put 'test-equalm 'scheme-indent-function 1))
+  (eval . (put 'test-equal 'scheme-indent-function 1))
+  (eval . (put 'test-eq 'scheme-indent-function 1))
+  (eval . (put 'call-with-input-string 'scheme-indent-function 1))
+  (eval . (put 'guard 'scheme-indent-function 1))
+  (eval . (put 'lambda* 'scheme-indent-function 1))
+  (eval . (put 'substitute* 'scheme-indent-function 1))
+  (eval . (put 'match-record 'scheme-indent-function 2))
+
+  ;; 'modify-phases' and its keywords.
+  (eval . (put 'modify-phases 'scheme-indent-function 1))
+  (eval . (put 'replace 'scheme-indent-function 1))
+  (eval . (put 'add-before 'scheme-indent-function 2))
+  (eval . (put 'add-after 'scheme-indent-function 2))
+
+  (eval . (put 'modify-services 'scheme-indent-function 1))
+  (eval . (put 'with-directory-excursion 'scheme-indent-function 1))
+  (eval . (put 'with-file-lock 'scheme-indent-function 1))
+  (eval . (put 'with-file-lock/no-wait 'scheme-indent-function 1))
+  (eval . (put 'with-profile-lock 'scheme-indent-function 1))
+
+  (eval . (put 'package 'scheme-indent-function 0))
+  (eval . (put 'origin 'scheme-indent-function 0))
+  (eval . (put 'build-system 'scheme-indent-function 0))
+  (eval . (put 'bag 'scheme-indent-function 0))
+  (eval . (put 'graft 'scheme-indent-function 0))
+  (eval . (put 'operating-system 'scheme-indent-function 0))
+  (eval . (put 'file-system 'scheme-indent-function 0))
+  (eval . (put 'manifest-entry 'scheme-indent-function 0))
+  (eval . (put 'manifest-pattern 'scheme-indent-function 0))
+  (eval . (put 'substitute-keyword-arguments 'scheme-indent-function 1))
+  (eval . (put 'with-store 'scheme-indent-function 1))
+  (eval . (put 'with-external-store 'scheme-indent-function 1))
+  (eval . (put 'with-error-handling 'scheme-indent-function 0))
+  (eval . (put 'with-mutex 'scheme-indent-function 1))
+  (eval . (put 'with-atomic-file-output 'scheme-indent-function 1))
+  (eval . (put 'call-with-compressed-output-port 'scheme-indent-function 2))
+  (eval . (put 'call-with-decompressed-port 'scheme-indent-function 2))
+  (eval . (put 'call-with-gzip-input-port 'scheme-indent-function 1))
+  (eval . (put 'call-with-gzip-output-port 'scheme-indent-function 1))
+  (eval . (put 'call-with-lzip-input-port 'scheme-indent-function 1))
+  (eval . (put 'call-with-lzip-output-port 'scheme-indent-function 1))
+  (eval . (put 'signature-case 'scheme-indent-function 1))
+  (eval . (put 'emacs-batch-eval 'scheme-indent-function 0))
+  (eval . (put 'emacs-batch-edit-file 'scheme-indent-function 1))
+  (eval . (put 'emacs-substitute-sexps 'scheme-indent-function 1))
+  (eval . (put 'emacs-substitute-variables 'scheme-indent-function 1))
+  (eval . (put 'with-derivation-narinfo 'scheme-indent-function 1))
+  (eval . (put 'with-derivation-substitute 'scheme-indent-function 2))
+  (eval . (put 'with-status-report 'scheme-indent-function 1))
+  (eval . (put 'with-status-verbosity 'scheme-indent-function 1))
+  (eval . (put 'with-build-handler 'scheme-indent-function 1))
+
+  (eval . (put 'mlambda 'scheme-indent-function 1))
+  (eval . (put 'mlambdaq 'scheme-indent-function 1))
+  (eval . (put 'syntax-parameterize 'scheme-indent-function 1))
+  (eval . (put 'with-monad 'scheme-indent-function 1))
+  (eval . (put 'mbegin 'scheme-indent-function 1))
+  (eval . (put 'mwhen 'scheme-indent-function 1))
+  (eval . (put 'munless 'scheme-indent-function 1))
+  (eval . (put 'mlet* 'scheme-indent-function 2))
+  (eval . (put 'mlet 'scheme-indent-function 2))
+  (eval . (put 'run-with-store 'scheme-indent-function 1))
+  (eval . (put 'run-with-state 'scheme-indent-function 1))
+  (eval . (put 'wrap-program 'scheme-indent-function 1))
+  (eval . (put 'with-imported-modules 'scheme-indent-function 1))
+  (eval . (put 'with-extensions 'scheme-indent-function 1))
+  (eval . (put 'with-parameters 'scheme-indent-function 1))
+  (eval . (put 'let-system 'scheme-indent-function 1))
+
+  (eval . (put 'with-database 'scheme-indent-function 2))
+  (eval . (put 'call-with-transaction 'scheme-indent-function 2))
+  (eval . (put 'with-statement 'scheme-indent-function 3))
+  (eval . (put 'call-with-retrying-transaction 'scheme-indent-function 2))
+  (eval . (put 'call-with-savepoint 'scheme-indent-function 1))
+  (eval . (put 'call-with-retrying-savepoint 'scheme-indent-function 1))
+
+  (eval . (put 'call-with-container 'scheme-indent-function 1))
+  (eval . (put 'container-excursion 'scheme-indent-function 1))
+  (eval . (put 'eventually 'scheme-indent-function 1))
+
+  (eval . (put 'call-with-progress-reporter 'scheme-indent-function 1))
+  (eval . (put 'with-temporary-git-repository 'scheme-indent-function 2))
+  (eval . (put 'with-environment-variables 'scheme-indent-function 1))
+  (eval . (put 'with-fresh-gnupg-setup 'scheme-indent-function 1))
+
+  (eval . (put 'with-paginated-output-port 'scheme-indent-function 1))
+
+  ;; This notably allows '(' in Paredit to not insert a space when the
+  ;; preceding symbol is one of these.
+  (eval . (modify-syntax-entry ?~ "'"))
+  (eval . (modify-syntax-entry ?$ "'"))
+  (eval . (modify-syntax-entry ?+ "'"))
+
+  (eval . (with-eval-after-load 'geiser-guile
+            (let ((root-dir
+                   (file-name-directory
+                    (locate-dominating-file default-directory ".dir-locals.el"))))
+              (unless (member root-dir geiser-guile-load-path)
+                (setq-local geiser-guile-load-path
+                            (cons root-dir geiser-guile-load-path))))))))
