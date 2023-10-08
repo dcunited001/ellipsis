4be55f345cd1c921dd6a3e132b989959b9a2bebd
David Conner <aionfork@gmail.com>
2023-10-08 00:19:08 -0400

    rearrange imports

--- ellipsis/packages/python-xyz.scm
+++ ellipsis/packages/python-xyz.scm
@@ -1,16 +1,14 @@
 (define-module (ellipsis packages python-xyz)
   #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix gexp)
-  #:use-module (guix utils)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages)
+  #:use-module (guix build-system pyproject)
+  #:use-module (guix build-system python)
   #:use-module (guix download)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix packages)
-  #:use-module (guix build-system python)
-  #:use-module (guix build-system pyproject)
-
-  #:use-module (gnu packages)
-  #:use-module (gnu packages python-xyz)
-
+  #:use-module (guix utils)
   #:use-module (srfi srfi-1)
 
   #:export (pyfoo))
