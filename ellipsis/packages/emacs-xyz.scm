37e4303c10dc8746a079ab1487c17f40559bfa3e
David Conner <aionfork@gmail.com>
2023-12-08 04:58:39 -0500

    remove x509 package

--- ellipsis/packages/emacs-xyz.scm
+++ ellipsis/packages/emacs-xyz.scm
@@ -7,35 +7,11 @@
   #:use-module (guix packages)
   #:use-module (guix build-system emacs)
   #:use-module (gnu packages)
-  #:use-module (gnu packages tls)
   #:use-module (srfi srfi-1))
 
-(define-public emacs-x509-mode
   (package
-    (name "emacs-x509-mode")
-    (version "20230430.1518")
-    (source
-     (origin
-       (method url-fetch)
-       (uri
-        (string-append "https://melpa.org/packages/x509-mode-" version ".tar"))
-       (sha256
-        (base32
-         "195gn14qn22aqclwz2i9w2qq9bxabwcasfkna1fj41jwv2bpbykc"))))
     (build-system emacs-build-system)
-    ;; (propagated-inputs `(("openssl" ,openssl)))
     (arguments
-     `(#:include
-       (cons* "^keywords.txt"
-              "^keyword\\+constant.txt"
-              "^constants.txt"
-              "^long-name.txt"
-              %default-include)))
-    (home-page "https://github.com/jobbflykt/x509-mode")
-    (synopsis "Major mode for viewing certificates, CRLs, and other PKI-related files")
-    (description "Major mode for viewing certificates, CRLs, and other PKI-related files.
-Uses OpenSSL for viewing PEM and DER encoded PKI entities.")
-    (license license:expat)))
 
 ;; TODO: maybe implement an ox-ssh package?
 
