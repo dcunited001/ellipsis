5cb2ab85b1ba94818b4c907df8adc882bbfa3e78
David Conner <aionfork@gmail.com>
2023-12-08 05:00:35 -0500

    add emacs-citar package (breaks on validation)

--- ellipsis/packages/emacs-xyz.scm
+++ ellipsis/packages/emacs-xyz.scm
@@ -7,11 +7,61 @@
   #:use-module (guix packages)
   #:use-module (guix build-system emacs)
   #:use-module (gnu packages)
+  #:use-module (gnu packages emacs-xyz)
   #:use-module (srfi srfi-1))
 
+(define-public emacs-citar
   (package
+    (name "emacs-citar")
+    (version "1.4.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/bdarcus/citar")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "07q94iplkx29lggrs5xfzj42rxfcn2cnbr90jgifk29jshcz30pv"))))
     (build-system emacs-build-system)
     (arguments
+     (list
+      #:tests? #true
+      #:test-command #~(list "emacs" "--batch" "-L" "."
+                             "-l" "test/citar-test.el"
+                             "-l" "test/citar-file-test.el"
+                             "-l" "test/citar-format-test.el"
+                             "-f" "ert-run-tests-batch-and-exit")
+      #:phases #~(modify-phases %standard-phases
+                   (add-before 'build 'set-home
+                     (lambda _
+                       (setenv "HOME" "/tmp")))
+                   ;;;; XXX: The following phase reports bogus errors. Suppress
+                   ;;;; it for now.
+                   ;;(delete 'validate-compiled-autoloads)
+
+		   )))
+    (propagated-inputs (list emacs-auctex
+                             emacs-citeproc-el
+                             emacs-embark
+                             emacs-org
+                             emacs-parsebib
+                             emacs-s))
+    (home-page "https://github.com/bdarcus/citar")
+    (synopsis "Emacs package to quickly find and act on bibliographic entries")
+    (description
+     "This package provides a completing-read front-end to browse and
+act on BibTeX, BibLaTeX, and CSL JSON bibliographic data, and LaTeX,
+markdown, and Org cite editing support.
+
+When used with Vertico (or Selectrum), Embark, and Marginalia, it
+provides similar functionality to helm-bibtex and ivy-bibtex: quick
+filtering and selecting of bibliographic entries from the minibuffer,
+and the option to run different commands against them.
+
+With Embark, it also makes available at-point actions in Org
+citations.")
+    (license license:gpl3+)))
 
 ;; TODO: maybe implement an ox-ssh package?
 
