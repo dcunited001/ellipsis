f2aae2a8635585cb09ba2104eeb043239c74d512
David Conner <aionfork@gmail.com>
2025-02-26 16:27:02 -0500

    getting ABI mismatch. try bumping hashes
    * maybe it's ~/.cache/guile or something?

--- env/dc/channels.scm
+++ env/dc/channels.scm
@@ -8,7 +8,7 @@
          (url "https://git.savannah.gnu.org/git/guix.git")
          (branch "master")
          (commit
-          "59b2a60d0041882d732e1766e28f0df5a1ef1ac1")
+          "3355de608cb2267435c2592fc7dc76a1dcc5c02d")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -19,7 +19,7 @@
          (url "https://git.sr.ht/~abcdw/rde")
          (branch "master")
          (commit
-          "b9e266c1ee0d6ffb688ff6634558d6f707109788")
+          "eaf0ec48cb06ee5dea1f86a502255e14c2b3973b")
          (introduction
           (make-channel-introduction
            "257cebd587b66e4d865b3537a9a88cccd7107c95"
