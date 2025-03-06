af590517d4ce73f529ec6857b5594d079288d743
David Conner <aionfork@gmail.com>
2025-03-05 22:12:33 -0500

    woops: copy and poopste

--- env/dc/channels.scm
+++ env/dc/channels.scm
@@ -14,13 +14,13 @@
             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
         (channel
          (name 'nonguix)
-         (url "https://git.savannah.gnu.org/git/guix.git")
+         (url "https://gitlab.com/nonguix/nonguix")
          (commit "d0b6099324cd47961064bd96ae830c4ca6d1bada")
          (introduction
           (make-channel-introduction
            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
            (openpgp-fingerprint
-            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
         (channel
          (name 'rde)
          (url "https://git.sr.ht/~abcdw/rde")
