5236c1f0cceb26cab9bba1c450065c5f59ba4024
David Conner <aionfork@gmail.com>
2025-03-05 22:10:48 -0500

    add nonguix channel

--- env/dc/channels.scm
+++ env/dc/channels.scm
@@ -6,20 +6,25 @@
   (list (channel
          (name 'guix)
          (url "https://git.savannah.gnu.org/git/guix.git")
-         (branch "master")
-         (commit
-          "3355de608cb2267435c2592fc7dc76a1dcc5c02d")
+         (commit "f785ff154c08e842b89145d9d47b20203d797e33")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
            (openpgp-fingerprint
             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
         (channel
+         (name 'nonguix)
+         (url "https://git.savannah.gnu.org/git/guix.git")
+         (commit "d0b6099324cd47961064bd96ae830c4ca6d1bada")
+         (introduction
+          (make-channel-introduction
+           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+           (openpgp-fingerprint
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+        (channel
          (name 'rde)
          (url "https://git.sr.ht/~abcdw/rde")
-         (branch "master")
-         (commit
-          "eaf0ec48cb06ee5dea1f86a502255e14c2b3973b")
+         (commit "8600655bbdc399b6fc682755d4d63f3709f562be")
          (introduction
           (make-channel-introduction
            "257cebd587b66e4d865b3537a9a88cccd7107c95"
