281bae81765540439663da159816d6a5f1acc816
David Conner <aionfork@gmail.com>
2025-04-22 23:19:30 -0400

    bump guix channels

---
+++ .config/guix/base-channels.scm
@@ -0,0 +1,40 @@
+(use-modules (guix ci)
+             (guix channels))
+
+(list
+ ;; %default-guix-channel
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ (channel
+  (name 'gocix)
+  (url "https://github.com/fishinthecalculator/gocix")
+  (branch "main")
+  ;; don't pin to commit (rapid dev cycle, channel may merge)
+  (introduction
+   (make-channel-introduction
+    "cdb78996334c4f63304ecce224e95bb96bfd4c7d"
+    (openpgp-fingerprint
+     "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix")
+  (introduction
+   (make-channel-introduction
+    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+    (openpgp-fingerprint
+     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+ (channel
+  (name 'rde)
+  (url "https://git.sr.ht/~abcdw/rde")
+  (commit "7648a495767990d625cd0ca242cd634aa39bcc58")
+  (introduction
+   (make-channel-introduction
+    "257cebd587b66e4d865b3537a9a88cccd7107c95"
+    (openpgp-fingerprint
+     "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
--- env/dc/channels.scm
+++ env/dc/channels.scm
@@ -6,16 +6,26 @@
   (list (channel
          (name 'guix)
          (url "https://git.savannah.gnu.org/git/guix.git")
-         (commit "ce086e31f0f5c78be76fd228dfd7718ff6df584e")
+         (commit "15562902da00c97910d918c7bf825d6fc6a9f84b")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
            (openpgp-fingerprint
             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
         (channel
+         (name 'gocix)
+         (url "https://github.com/fishinthecalculator/gocix")
+         ;; (branch "main")
+         (commit "8d54bdeb5fe0cdad6dfbfe6fe3bc59037216869d")
+         (introduction
+          (make-channel-introduction
+           "cdb78996334c4f63304ecce224e95bb96bfd4c7d"
+           (openpgp-fingerprint
+            "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+        (channel
          (name 'nonguix)
          (url "https://gitlab.com/nonguix/nonguix")
-         (commit "a96e2451bda5aaf9b48339edee392c6a3017d730")
+         (commit "e899121adbaa13cfcaeae7a5c24921bffa645771")
          (introduction
           (make-channel-introduction
            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -24,7 +34,7 @@
         (channel
          (name 'rde)
          (url "https://git.sr.ht/~abcdw/rde")
-         (commit "598c39bfe40e8ab2643be82e14504e932e3db102")
+         (commit "7648a495767990d625cd0ca242cd634aa39bcc58")
          (introduction
           (make-channel-introduction
            "257cebd587b66e4d865b3537a9a88cccd7107c95"
