52c6596ab5d7036d9d299bcd21a0c978a06ed1c0
David Conner <aionfork@gmail.com>
2025-05-06 01:37:17 -0400

    update dir-locals for common-lisp

--- .dir-locals.el
+++ .dir-locals.el
@@ -52,7 +52,45 @@
  (shell-mode
   (eval . (add-hook 'shell-mode-hook 'guix-build-log-minor-mode)))
 
- (c-mode          . ((c-file-style . "gnu")))
+ (c-mode
+  . ((c-file-style . "gnu")))
+ (lisp-mode
+  . ((sly-contribs nil)
+     (eval . (cl-flet ((enhance-imenu-lisp
+                         (&rest keywords)
+                         (dolist (keyword keywords)
+                           (let ((prefix (when (listp keyword) (cl-second keyword)))
+                                 (keyword (if (listp keyword)
+                                              (cl-first keyword)
+                                            keyword)))
+                             (add-to-list
+                              'lisp-imenu-generic-expression
+                              (list (purecopy (concat (capitalize keyword)
+                                                      (if (string= (substring-no-properties keyword -1) "s")
+                                                          "es"
+                                                        "s")))
+                                    (purecopy (concat "^\\s-*("
+                                                      (regexp-opt
+                                                       (list (if prefix
+                                                                 (concat prefix "-" keyword)
+                                                               keyword)
+                                                             (concat prefix "-" keyword))
+                                                       t)
+                                                      "\\s-+\\(" lisp-mode-symbol-regexp "\\)"))
+                                    2))))))
+               ;; This adds the argument to the list of imenu known keywords.
+               (enhance-imenu-lisp
+                '("bookmarklet-command" "define")
+                '("class" "define")
+                '("command" "define")
+                '("ffi-method" "define")
+                '("ffi-generic" "define")
+                '("function" "define")
+                '("internal-page-command" "define")
+                '("internal-page-command-global" "define")
+                '("mode" "define")
+                '("parenscript" "define")
+                "defpsmacro")))))
 
  (scheme-mode
   (eval . (add-hook 'scheme-mode-hook 'guix-devel-mode))
