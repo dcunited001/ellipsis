830930c2b98f4386462f9535121654ee812f4768
David Conner <aionfork@gmail.com>
2025-05-10 11:27:28 -0400

    asdf

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -127,6 +127,7 @@ Guix channel.")
 (global-set-key (kbd "C-<prior>") #'tab-bar-switch-to-tab)
 (global-set-key (kbd "C-<next>") #'tab-bar-switch-to-recent-tab)
 (global-set-key (kbd "C-M-S-<return>") #'duplicate-line)
+(global-set-key (kbd "C-x M-f") #'find-file-at-point)
 
 ;;;;; Help Map (native)
 
