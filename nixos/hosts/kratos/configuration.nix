5f5e0415d1bc64c6970a04e2aa4352f7bac6360e
David Conner <aionfork@gmail.com>
2025-07-29 22:57:55 -0400

    nix: retain 15 boot entries

--- nixos/hosts/kratos/configuration.nix
+++ nixos/hosts/kratos/configuration.nix
@@ -27,6 +27,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader = {
     systemd-boot.enable = true;
+    systemd-boot.configurationLimit = 15;
     efi.canTouchEfiVariables = true;
   };
 
