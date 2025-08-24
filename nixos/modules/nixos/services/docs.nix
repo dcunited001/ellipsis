74d4cad9380af2b3c1c3cb43dd71af6ab078df86
David Conner <aionfork@gmail.com>
2025-08-24 03:52:04 -0400

    update docs defaults

--- nixos/modules/nixos/services/docs.nix
+++ nixos/modules/nixos/services/docs.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
+  # if it's not a server, add manix (probably requires nixos docs)
+  # environment.systemPackages = with pkgs; [ manix ];
+
+  # defaults
+  documentation.man.enable = true;
+  documentation.man.generateCaches = false;
 
 }
