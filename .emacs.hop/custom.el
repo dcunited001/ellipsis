f7672bb833b9d72f2ff51eff73d19df883129739
David Conner <aionfork@gmail.com>
2025-08-26 04:41:13 -0400

    emacs: yeh... gonna remove custom.el and manage a template
    * I don't want dir-locals.el paths in here
    
    * i mainly need a list of packages to install and maybe some
      other adhoc customizations

--- .emacs.hop/custom.el
+++ .emacs.hop/custom.el
@@ -5,21 +5,18 @@
  ;; If there is more than one, they won't work right.
  '(custom-file "~/.emacs.d/custom.el")
  '(package-selected-packages
-   '(0x0 a ace-window cape consult consult-dir corfu corfu-terminal
-		 ef-themes embark embark-consult json-mode lispy magit
-		 marginalia markdown-mode multiple-cursors nix-mode
-		 no-littering orderless toml-mode vertico x509-mode xkb-mode
-		 xkb-mode yuck-mode))
+   '(0x0 a ace-window cape consult consult-dir corfu corfu-terminal ef-themes
+		 embark embark-consult json-mode lispy magit marginalia markdown-mode
+		 multiple-cursors nix-mode no-littering orderless toml-mode vertico
+		 x509-mode xkb-mode xkb-mode yuck-mode))
  '(safe-local-variable-values
    '((eval let
 		   ((root-dir-unexpanded
-			 (locate-dominating-file default-directory
-									 ".dir-locals.el")))
+			 (locate-dominating-file default-directory ".dir-locals.el")))
 		   (when root-dir-unexpanded
 			 (let*
 				 ((root-dir
-				   (file-local-name
-					(expand-file-name root-dir-unexpanded)))
+				   (file-local-name (expand-file-name root-dir-unexpanded)))
 				  (dc-dir
 				   (directory-file-name
 					(file-local-name
@@ -32,18 +29,14 @@
 				   (or (and (bound-and-true-p guix-pulled-profile))
 					   (expand-file-name ".config/guix/current"
 										 (getenv "HOME"))))
-				  (guix-lp
-				   (expand-file-name "share/guile/site/3.0" guix-pr))
+				  (guix-lp (expand-file-name "share/guile/site/3.0" guix-pr))
 				  (guix-lcp
-				   (expand-file-name "lib/guile/3.0/site-ccache"
-									 guix-pr)))
+				   (expand-file-name "lib/guile/3.0/site-ccache" guix-pr)))
 			   (unless (boundp 'geiser-guile-load-path)
 				 (defvar geiser-guile-load-path 'nil))
-			   (make-local-variable 'geiser-guile-load-path)
-			   (require 'cl-lib)
+			   (make-local-variable 'geiser-guile-load-path) (require 'cl-lib)
 			   (cl-dolist
-				   (pathdir
-					(list guix-lp guix-lcp ellipsis-dir dc-dir))
+				   (pathdir (list guix-lp guix-lcp ellipsis-dir dc-dir))
 				 (cl-pushnew pathdir geiser-guile-load-path :test
 							 #'string-equal)))))
 	 (eval add-to-list 'completion-ignored-extensions ".go")
