a02833cfa6eb6ecc247650e7c87bb23a3a7c279f
David Conner <aionfork@gmail.com>
2025-09-04 12:05:07 -0400

    nixos: updates to pipewire

--- nixos/modules/nixos/desktop/pipewire.nix
+++ nixos/modules/nixos/desktop/pipewire.nix
@@ -9,9 +9,46 @@
     pulse.enable = true;
     jack.enable = true;
 
+    extraConfig.pipewire."91-null-sinks" = {
+      "context.objects" = [
+        # pipewire seems to already have a dummy driver
+        #
+        # {
+        #   factory = "spa-node-factory";
+        #   args = {
+        #     "factory.name" = "support.node.driver";
+        #     "node.name" = "Dummy-Driver";
+        #     "priority.driver" = 8000;
+        #   };
+        # }
+        {
+          factory = "adapter";
+          args = {
+            "factory.name" = "support.null-audio-sink";
+            "node.name" = "Microphone-Proxy";
+            "node.description" = "Microphone";
+            "media.class" = "Audio/Source/Virtual";
+            "audio.position" = "MONO";
+          };
+        }
+        {
+          factory = "adapter";
+          args = {
+            "factory.name" = "support.null-audio-sink";
+            "node.name" = "Main-Output-Proxy";
+            "node.description" = "Main Output";
+            "media.class" = "Audio/Sink";
+            "audio.position" = "FL,FR";
+          };
+        }
+      ];
+    };
+
+    # low-latency settings
     extraConfig.pipewire."92-low-latency" = {
       "context.properties" = {
         "default.clock.rate" = 48000;
+        # quantum: buffer size
         "default.clock.quantum" = 256;
         "default.clock.min-quantum" = 256;
         "default.clock.max-quantum" = 256;
@@ -28,6 +65,15 @@
           pulse.max.quantum = "256/48000";
         };
       }];
+    };
+
+    wireplumber.extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
     };
   };
 }
