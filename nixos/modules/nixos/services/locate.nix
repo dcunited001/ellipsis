48bacfb65a8ff4628e8e30e4aed92b2712817010
David Conner <aionfork@gmail.com>
2025-09-13 04:54:19 -0400

    nixos: add locate service

---
+++ nixos/modules/nixos/services/locate.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }: {
+  services.locate.enable = true;
+  services.locate = {
+    prunePaths = [
+      "/afs"
+      "/gnu/store"
+      "/net"
+      "/media"
+      "/mnt"
+      "/sfs"
+      "/tmp"
+      "/udev"
+      "/root"
+      "/proc"
+    ];
+    pruneBindMounts = true;
+    # others: /sys /run
+
+    # defaults:
+    # pruneNames = [ ".bzr" ".cache" ".git" ".hg" ".svn" ];
+    # pruneFS = [ "looks good" ];
+    # extraFlags = []; # arch doesn't pass anything else
+    # prunePaths = [ "/tmp" "/var/tmp" "/var/cache" "/var/lock" "/var/run" "/var/spool"
+    #   "/nix/store" "/nix/var/log/nix" ];
+  };
+}
