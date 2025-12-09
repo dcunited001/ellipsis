52900a9e3fcda7ca9adb5e8b4eb77007f0b08af5
David Conner <aionfork@gmail.com>
2025-12-09 02:01:34 -0500

    hypr: trying to add an overlay to fix hyprlang

--- nixos/overlays/default.nix
+++ nixos/overlays/default.nix
@@ -23,6 +23,7 @@ let
         sha256 = "sha256-V7lRjvq6vFq0hR4fNYFznahMo/NayVHK18ELKL4Q0To=";
       };
     };
+
     # hyprland = prev.hyprland.overrideAttrs {
     #   mesa = final.unstable.mesa;
     # };
@@ -31,11 +32,41 @@ let
     # };
     #
   };
+
+  hyprland-packages = final: prev: {
+    # hyprland = import inputs.hyprland.packages.${prev.stdenv.hostPlatform.system} {
+    hyprland = import "${inputs.hyprland.outPath}/nix" {
+      inherit (final) system;
+      config.allowUnfree = true;
+      overlays = [
+        (hyprland_final: hyprland_prev: {
+          hyprlang = hyprland_prev.hyprlang.overrideAttrs (
+            previousAttrs:
+            let
+              version = "0.6.6";
+            in
+            rec {
+              inherit version;
+              src = prev.fetchFromGitHub {
+                owner = "hyprwm";
+                repo = "hyprlang";
+                rev = "0.6.6";
+                sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+              };
+            }
+          );
+        })
+      ];
+    };
+  };
 in
 {
   default =
     final: prev:
-    (additions final prev) // (modifications final prev) // (linuxModifications final prev);
+    (additions final prev)
+    // (modifications final prev)
+    // (linuxModifications final prev)
+    // (hyprland-packages final prev);
 }
 
 # stable-packages = final: prev: {
