4d71f8837759aa77826799773d7fde2984302eb5
David Conner <aionfork@gmail.com>
2025-12-11 03:38:53 -0500

    mail: autostart both mail and calendar

--- nixos/home/dc/thunderbird.nix
+++ nixos/home/dc/thunderbird.nix
@@ -14,13 +14,13 @@
           Actions=profile-manager-window;compose-message;calendar;open-address-book;open-file;keymanager;settings
           Categories=Network;Chat;Email;Feed;GTK;News
           Comment=Read and write e-mails or RSS feeds, or manage tasks on calendars.
-          Exec=thunderbird --name org.mozilla.Thunderbird
+          Exec=thunderbird --name org.mozilla.Thunderbird -mail -calendar
           GenericName=Email Client
           Icon=thunderbird
           # Icon=org.mozilla.Thunderbird
           Keywords=mail;email;e-mail;messages;rss;calendar;address book;addressbook;chat
           MimeType=message/rfc822;x-scheme-handler/mailto;text/calendar;text/vcard;text/x-vcard;x-scheme-handler/webcal;x-scheme-handler/webcals;x-scheme-handler/mid;
-          Name=Thunderbird
+          Name=Thunderbird (Hjem)
           StartupNotify=true
           StartupWMClass=org.mozilla.Thunderbird
           Terminal=false
@@ -40,7 +40,7 @@
           # Icon=org.mozilla.Thunderbird
           Keywords=mail;email;e-mail;messages;rss;calendar;address book;addressbook;chat
           MimeType=message/rfc822;x-scheme-handler/mailto;text/calendar;text/vcard;text/x-vcard;x-scheme-handler/webcal;x-scheme-handler/webcals;x-scheme-handler/mid;
-          Name=Thunderbird
+          Name=Thunderbird (Hjem)
           StartupNotify=true
           StartupWMClass=org.mozilla.Thunderbird
           Terminal=false
@@ -49,32 +49,32 @@
 
           [Desktop Action profile-manager-window]
           Exec=thunderbird --ProfileManager
-          Name=Profile Manager
+          Name=Profile Manager (Hjem)
 
           [Desktop Action compose-message]
           Exec=thunderbird -compose
-          Name=Compose Message
+          Name=Compose Message (Hjem)
 
           [Desktop Action calendar]
           Exec=thunderbird -calendar
-          Name=Calendar
+          Name=Calendar (Hjem)
 
           [Desktop Action open-file]
           Exec=thunderbird -file %U
-          Name=Open File (.ical)
+          Name=Open File (.ical) (Hjem)
           # TODO ensure .ical mimetype handles this
 
           [Desktop Action open-address-book]
           Exec=thunderbird -addressbook
-          Name=Address Book
+          Name=Address Book (Hjem)
 
           [Desktop Action settings]
           Exec=thunderbird -settings
-          Name=Settings
+          Name=Settings (Hjem)
 
           [Desktop Action keymanager]
           Exec=thunderbird -keymanager
-          Name=Manage OpenPGP Keys
+          Name=Manage OpenPGP Keys (Hjem)
         '';
         clobber = true;
       };
