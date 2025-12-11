26bb73c78b8c0275c0d99d6422778fc4eb048fd2
David Conner <aionfork@gmail.com>
2025-12-11 03:30:29 -0500

    nix/mail: remove overlay, copy for other distros

--- nixos/overlays/default.nix
+++ nixos/overlays/default.nix
@@ -33,40 +33,40 @@ let
     #
   };
 
-  hyprland-packages = final: prev: {
-    # hyprland = import inputs.hyprland.packages.${prev.stdenv.hostPlatform.system} {
-    hyprland = import "${inputs.hyprland.outPath}/nix" {
-      inherit (final) system;
-      config.allowUnfree = true;
-      overlays = [
-        (hyprland_final: hyprland_prev: {
-          hyprlang = hyprland_prev.hyprlang.overrideAttrs (
-            previousAttrs:
-            let
-              version = "0.6.6";
-            in
-            rec {
-              inherit version;
-              src = prev.fetchFromGitHub {
-                owner = "hyprwm";
-                repo = "hyprlang";
-                rev = "0.6.6";
-                sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-              };
-            }
-          );
-        })
-      ];
-    };
-  };
+  # apparantly a very bad way to do this;
+  #
+  # hyprland-packages = final: prev: {
+  #   # hyprland = import inputs.hyprland.packages.${prev.stdenv.hostPlatform.system} {
+  #   hyprland = import "${inputs.hyprland.outPath}/nix" {
+  #     inherit (final) system;
+  #     config.allowUnfree = true;
+  #     overlays = [
+  #       (hyprland_final: hyprland_prev: {
+  #         hyprlang = hyprland_prev.hyprlang.overrideAttrs (
+  #           previousAttrs:
+  #           let
+  #             version = "0.6.6";
+  #           in
+  #           rec {
+  #             inherit version;
+  #             src = prev.fetchFromGitHub {
+  #               owner = "hyprwm";
+  #               repo = "hyprlang";
+  #               rev = "0.6.6";
+  #               sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  #             };
+  #           }
+  #         );
+  #       })
+  #     ];
+  #   };
+  # };
 in
 {
   default =
     final: prev:
-    (additions final prev)
-    // (modifications final prev)
-    // (linuxModifications final prev)
-    // (hyprland-packages final prev);
+    (additions final prev) // (modifications final prev) // (linuxModifications final prev);
+  # // (hyprland-packages final prev);
 }
 
 # stable-packages = final: prev: {
