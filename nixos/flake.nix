3e8fb231424b6e4c7f2c478700d16a43e94177c5
David Conner <aionfork@gmail.com>
2026-03-05 05:44:21 -0500

    nixos: rollback (thought i'd built on 0.1.942631)

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -103,7 +103,9 @@
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
 
     # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
-    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1.957146";
+    # fails (guile-zlib checks): 0.1.948651, 0.1.942631, 0.1.957146
+
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1.921317";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
