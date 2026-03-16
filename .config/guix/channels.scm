2368f626df7162ee74fa1bd8ea1fc1d104a6456d
David Conner <aionfork@gmail.com>
2026-03-16 00:47:37 -0400

    Guix/Doom: upgrade

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,55 +1,50 @@
 (list (channel
-        (name 'guix)
-        (url "https://codeberg.org/guix/guix.git")
-        (branch "master")
-        (commit
-          "5429b9a1f9756d78564e905bcc9721bf9bd73976")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+       (name 'guix)
+       (url "https://codeberg.org/guix/guix.git")
+       (branch "master")
+       (commit "d6567a6a1773b8ec7d8ded5ec793c156812cd819")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
-        (name 'sops-guix)
-        (url "https://github.com/fishinthecalculator/sops-guix")
-        (branch "main")
-        (commit
-          "909522979fb1e26cc8d73e486d21b1d0b7f43cf0")
-        (introduction
-          (make-channel-introduction
-            "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
-            (openpgp-fingerprint
-              "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+       (name 'sops-guix)
+       (url "https://github.com/fishinthecalculator/sops-guix")
+       (branch "main")
+       (commit "5a88726e1be11e2df0bb85b597a1f597e334e0b7")
+       (introduction
+        (make-channel-introduction
+         "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+         (openpgp-fingerprint
+          "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
       (channel
-        (name 'gocix)
-        (url "https://github.com/fishinthecalculator/gocix")
-        (branch "main")
-        (commit
-          "04303c0773fd14644f8e361c4fffcd527fd3dec5")
-        (introduction
-          (make-channel-introduction
-            "cdb78996334c4f63304ecce224e95bb96bfd4c7d"
-            (openpgp-fingerprint
-              "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+       (name 'gocix)
+       (url "https://github.com/fishinthecalculator/gocix")
+       (branch "main")
+       (commit "04303c0773fd14644f8e361c4fffcd527fd3dec5")
+       (introduction
+        (make-channel-introduction
+         "cdb78996334c4f63304ecce224e95bb96bfd4c7d"
+         (openpgp-fingerprint
+          "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
       (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (branch "master")
-        (commit
-          "48a8706d44040cc7014f36873dbd834c048aadd3")
-        (introduction
-          (make-channel-introduction
-            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-            (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (commit "7d14e819fc7b2681240023b9e4c001fc3f5cba04")
+       (introduction
+        (make-channel-introduction
+         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+         (openpgp-fingerprint
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
-        (name 'rde)
-        (url "https://git.sr.ht/~abcdw/rde")
-        (branch "master")
-        (commit
-          "31da7c20f745e2dceda818ae6831de9dc1b7faec")
-        (introduction
-          (make-channel-introduction
-            "257cebd587b66e4d865b3537a9a88cccd7107c95"
-            (openpgp-fingerprint
-              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
+       (name 'rde)
+       (url "https://git.sr.ht/~abcdw/rde")
+       (branch "master")
+       (commit "e18c720bd54adcd0f4c8ff71075cfe36117d82b3")
+       (introduction
+        (make-channel-introduction
+         "257cebd587b66e4d865b3537a9a88cccd7107c95"
+         (openpgp-fingerprint
+          "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
