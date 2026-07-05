eee1dbe284a421228bdfe3835d1503ddbfc2d958
David Conner <aionfork@gmail.com>
2026-07-05 14:05:42 -0400

    guix: fix build from tamil cookbook

--- env/dc-configs/guix/channels.scm
+++ env/dc-configs/guix/channels.scm
@@ -11,55 +11,55 @@
   #:export (core-channels))
 
 (define core-channels
-  (list (channel
-         (name 'guix)
-         (url "https://codeberg.org/guix/guix.git")
-         (branch "master")
-         (commit "c86d515f948c12c14c5837a1df914f9394e8c120")
-         (introduction
-          (make-channel-introduction
-           "9edb3f66fd807b096b48283debdcddccfea34bad"
-           (openpgp-fingerprint
-            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-        (channel
-         (name 'sops-guix)
-         (url "https://github.com/fishinthecalculator/sops-guix")
-         (branch "main")
-         (commit "c53e27e533836ea8595626ba6796dee5362f8c4a")
-         (introduction
-          (make-channel-introduction
-           "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
-           (openpgp-fingerprint
-            "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
-        (channel
-         (name 'gocix)
-         (url "https://github.com/fishinthecalculator/gocix")
-         (branch "main")
-         (commit "7257cb326e177de4cd0180325c1cdba743fe43e5")
-         (introduction
-          (make-channel-introduction
-           "cdb78996334c4f63304ecce224e95bb96bfd4c7d"
-           (openpgp-fingerprint
-            "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
-        (channel
-         (name 'nonguix)
-         (url "https://gitlab.com/nonguix/nonguix")
-         (branch "master")
-         (commit "3ed7c207c59dde11a97db483cad4c96eae1a10c4")
-         (introduction
-          (make-channel-introduction
-           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-           (openpgp-fingerprint
-            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-        (channel
-         (name 'rde)
-         (url "https://git.sr.ht/~abcdw/rde")
-         (branch "master")
-         (commit "740d510ecd697a966be1d10da2a18162a4234cb3")
-         (introduction
-          (make-channel-introduction
-           "257cebd587b66e4d865b3537a9a88cccd7107c95"
-           (openpgp-fingerprint
-            "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))))
+ (list (channel
+       (name 'guix)
+       (url "https://codeberg.org/guix/guix.git")
+       (branch "master")
+       (commit "74a5cdd8c506dfbb080253fbfb6f97afe3d5321a")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+       (name 'sops-guix)
+       (url "https://github.com/fishinthecalculator/sops-guix")
+       (branch "main")
+       (commit "c53e27e533836ea8595626ba6796dee5362f8c4a")
+       (introduction
+        (make-channel-introduction
+         "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+         (openpgp-fingerprint
+          "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+      (channel
+       (name 'gocix)
+       (url "https://github.com/fishinthecalculator/gocix")
+       (branch "main")
+       (commit "5cbc7d0cb911dd27eb364d350ac5a1ef43308316")
+       (introduction
+        (make-channel-introduction
+         "cdb78996334c4f63304ecce224e95bb96bfd4c7d"
+         (openpgp-fingerprint
+          "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+      (channel
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (commit "66ab7fff7a4ee0592c708651556ef3805c85068f")
+       (introduction
+        (make-channel-introduction
+         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+         (openpgp-fingerprint
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+       (name 'rde)
+       (url "https://git.sr.ht/~abcdw/rde")
+       (branch "master")
+       (commit "6ccd368e0a0484e724205f9d608d2eb1ab706ac3")
+       (introduction
+        (make-channel-introduction
+         "257cebd587b66e4d865b3537a9a88cccd7107c95"
+         (openpgp-fingerprint
+          "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))))
 
 core-channels
