e8f8a5c3b05bc809f632a08b285b3e41df9a142c
David Conner <aionfork@gmail.com>
2026-08-10 11:31:47 -0400

    hypr: merge changes; nix: add guix pubkey

--- .config/hypr/workspaces/discord.lua
+++ .config/hypr/workspaces/discord.lua
@@ -2,16 +2,16 @@
 
 local ws_discord = { name = "discord", mod = "SUPER + ALT", key = "Delete" }
 ws_discord.size = "90% 90%"
-ws_discord.launch = "uwsm app -- chromium --app=\"https://discord.com/channels/@me\""
+ws_discord.launch = 'uwsm app -- chromium --app="https://discord.com/channels/@me"'
 
 --** Binds
 dc.ws.binds_special(ws_discord.mod, ws_discord.key, ws_discord.name)
 
 --** Workspace
 hl.workspace_rule({
-    workspace = "special:" .. ws_discord.name,
+    workspace = ws_discord.name,
     border_size = 5,
-    on_created_empty = "[float] " .. ws_discord.launch
+    on_created_empty = "[float] " .. ws_discord.launch,
 })
 
 --** Rules
@@ -21,8 +21,8 @@ local discord_classes = { "(chrome-discord.com.*)", "discord", "vesktop" }
 for i, klass in ipairs(discord_classes) do
     hl.window_rule({
         match = { class = klass },
-        workspace = "special:" .. ws_discord.name,
+        workspace = ws_discord.name,
         size = ws_discord.size,
-        move = "10% 10%"
+        move = "10% 10%",
     })
 end
