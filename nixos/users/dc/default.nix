26a81293674f84d1153039d04b72416fac0ad0ea
David Conner <aionfork@gmail.com>
2026-08-10 02:29:11 -0400

    nixos: add omarchy-quattro to check quickshell

--- nixos/users/dc/default.nix
+++ nixos/users/dc/default.nix
@@ -9,6 +9,9 @@ let
   omarchy-scripts = (
     pkgs.callPackage (lib.custom.relativeToRoot "pkgs/dots/omarchy-scripts/package.nix") { }
   );
+  omarchy-quattro = (
+    pkgs.callPackage (lib.custom.relativeToRoot "pkgs/common/omarchy-quattro/package.nix") { }
+  );
   # dcstaticdots
   chrome = inputs.browser-previews.packages.${pkgs.stdenv.hostPlatform.system}.google-chrome;
   # (these packages don't have outputs from the direct hyprland output)
@@ -36,6 +39,9 @@ in
   environment.sessionVariables.XDG_DATA_DIRS = [ "$HOME/.local/share/flatpak/exports/share" ];
   environment.sessionVariables.ORG_DIRECTORY = [ "$HOME/org" ];
   environment.sessionVariables.SYSTEMD_EDITOR = [ "vim" ];
+
+  environment.sessionVariables.OMARCHY_PATH = "$HOME/.nix-profile/share/omarchy";
+  environment.pathsToLink = [ "/share/omarchy" ];
 
   users.users.dc = {
     uid = 1000;
@@ -83,6 +89,7 @@ in
 
       # CUSTOM: omarchy
       omarchy-scripts # set BROWSER in uwsm startup or webapps don't work
+      omarchy-quattro
       gum
 
       # CLI
